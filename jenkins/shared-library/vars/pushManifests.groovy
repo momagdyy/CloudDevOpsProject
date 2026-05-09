9fb51c56a1a97038925feecfd4d2ea539dad0451
momagdyy <mohamed.magdy782002@gmail.com>
2026-05-09 14:46:03 +0300

    Fix pushManifests branch issue

--- jenkins/shared-library/vars/pushManifests.groovy
+++ jenkins/shared-library/vars/pushManifests.groovy
@@ -2,6 +2,7 @@ def call(String repoUrl, String imageTag) {
     sh """
         git config user.email "jenkins@jenkins.com"
         git config user.name "Jenkins"
+        git checkout -B main
         git add kubernetes/deployment.yaml
         git commit -m "Update image tag to ${imageTag}"
         git push ${repoUrl} main
